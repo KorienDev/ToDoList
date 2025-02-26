b82f547cf1a37065aa49c9ebfbcfd48697e2c9f2
koriendev <koriendev@gmail.com>
2025-02-26 18:31:28 +0100

    Selección de usuario y randomicer

--- ToDoList/src/app/user/user.component.ts
+++ ToDoList/src/app/user/user.component.ts
@@ -1,15 +1,27 @@
 import { Component } from '@angular/core';
+import { DUMMY_USERS } from '../dummy-users';
+const random_index = Math.floor(Math.random() * DUMMY_USERS.length);
+
 
 @Component({
   selector: 'app-user',
-  imports: [  ],
+  imports: [ ],
   templateUrl: './user.component.html',
   styleUrl: './user.component.css'
 })
 export class UserComponent {
-  //   user = DUMMY_USERS;
+  
+  selectedUser = DUMMY_USERS[random_index];
+  
+  onSelectUser(){
+    const random_index = Math.floor(Math.random() * DUMMY_USERS.length);
+    console.log( this.selectedUser.id, this.selectedUser.avatar);
+    this.selectedUser = DUMMY_USERS[random_index];
+  }
 
-  // getimg(){
-  //    return this.user.avatar;
+  // get imagePath() {
+  //   return this.selectedUser.avatar;
   // }
+
+
 }
